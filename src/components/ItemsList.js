c5937d84065b4bc89edcd1b12e1b785ea32924a9
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-22 18:50:15 +0530

    update listitems

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,3 +1,5 @@
+import { ITEMS_IMG } from "../utils/constants";
+
 const ItemsList=({item})=>{
     
     console.log(item?.itemCards)
@@ -13,7 +15,28 @@ const ItemsList=({item})=>{
     return(
         <div>
           
-           <p>{ item?.itemCards.map((e)=><li>{e?.card?.info?.name}</li>)}</p>
+           <p >{ item?.itemCards.map((e)=>
+                <div className="">
+                    <div className="flex justify-between py-5 border-gray-500 border-b-2" >
+                    <div className="items-center flex">
+                    <span className="font-black">{e?.card?.info?.name }</span>
+                    <span> - ₹ {e?.card?.info?.price}</span>
+                    </div>
+
+                   <div className="items-end justify-center flex">
+                   
+                   <button className="border-green-600 border text-green-500 font-semibold  absolute mt-5 bg-white p-1 px-5 rounded-xl">Add</button>
+                    { ITEMS_IMG+e.card.info.imageId && <img className="w-28 h-24 rounded-lg" src={ ITEMS_IMG+e.card.info.imageId} alt="img"></img>}
+
+                    
+                   
+                   </div>
+                    </div>
+
+                  
+                </div>
+                )}</p>
+           
         </div>
     )
 }
