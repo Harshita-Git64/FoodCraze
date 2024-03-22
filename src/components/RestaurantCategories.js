72c0e4db4c7204bb5adacaa49a40f4175b1de140
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-22 18:55:00 +0530

    Implement Accordian feature

--- src/components/RestaurantCategories.js
+++ src/components/RestaurantCategories.js
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import ItemsList from "./ItemsList";
 
 const RestaurantCategories=(props)=>{
    // console.log(props?.data?.title)
+   const[showItems,SetShowItems]=useState(false)
+   function ShowItemLists(){
+   
+    if(!showItems){
+        console.log("close")
+        SetShowItems(true)
+    }
+    else{
+        console.log("open")
+        SetShowItems(false)
+    }
+   // SetShowItems(!showItems);
+   }
     return(
         <div>
            
-            <div className="bg-gray-300 my-4 mx-auto shadow-xl px-5 py-1 items-center hover:cursor-pointer" onClick={()=>{
-               
-                
-            }}>
-                <div className="flex justify-between">
+            <div className="bg-gray-200 my-4 mx-64 shadow-xl px-2 py-1 items-center" >
+                <div className="flex justify-between  hover:cursor-pointer bg-gray-300" onClick={()=>ShowItemLists()}>
                 <span className="font-bold text-2xl">{props?.data?.title} ({props?.data?.itemCards.length})</span>
-                <span >🔽</span>
+                <span > {showItems?"🔼":"🔽"}</span>
                 </div>
 
-                <div> <ItemsList item={props?.data}/></div>
+                {showItems && <ItemsList item={props?.data}/>}
             </div>
            
            
