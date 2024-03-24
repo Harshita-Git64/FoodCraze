c608879cf3c1e0678ede925a6e284b0e4daa24a3
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-24 23:31:53 +0530

    implement dropdownlist

--- src/components/RestaurantCategories.js
+++ src/components/RestaurantCategories.js
@@ -1,31 +1,25 @@
 import { useState } from "react";
 import ItemsList from "./ItemsList";
 
-const RestaurantCategories=(props)=>{
+const RestaurantCategories=({data,showItems,SetShowIndex})=>{
    // console.log(props?.data?.title)
-   const[showItems,SetShowItems]=useState(false)
+  
    function ShowItemLists(){
    
-    if(!showItems){
-        console.log("close")
-        SetShowItems(true)
-    }
-    else{
-        console.log("open")
-        SetShowItems(false)
-    }
-   // SetShowItems(!showItems);
+    SetShowIndex();
+    //SetShowItems(!showItems)
+   // console.log(showItems);
+   
    }
     return(
         <div>
-           
             <div className="bg-gray-200 my-4 mx-64 shadow-xl px-2 py-1 items-center" >
                 <div className="flex justify-between  hover:cursor-pointer bg-gray-300" onClick={()=>ShowItemLists()}>
-                <span className="font-bold text-2xl">{props?.data?.title} ({props?.data?.itemCards.length})</span>
+                <span className="font-bold text-2xl">{data.title} ({data.itemCards.length})</span>
                 <span > {showItems?"🔼":"🔽"}</span>
                 </div>
 
-                {showItems && <ItemsList item={props?.data}/>}
+                {showItems && <ItemsList item={data}/>}
             </div>
            
            
