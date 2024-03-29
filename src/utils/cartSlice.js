2cb6b45e780821b066782c266db50d7101d45597
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-29 23:58:22 +0530

    Add remove cart item reducer

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,23 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice=createSlice({
-
-    name:'cartlist',
-    initialState:{
-        items:[1,2,3]
+const cartSlice = createSlice({
+  name: "cartlist",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
     },
-    reducers:{
-        addItem:(state,action)=>{
-        state.items.push(action.payload);
-        },
-        removeItem:(state)=>{
-            state.items.pop();
-        },
-        clearCart:(state)=>{
-            state.items.length=0;
-        }
-    }
+    removeItem: (state,action) => {
+      console.log(action.payload)
+      const index=state.items.findIndex((item)=>item.id===action.payload.id)
+      if(index>=0){
+      state.items.splice(index,1)
+      }
+      console.log(index)
+      //state.items.filter((rem)=>rem.card.info.id!=action.payload)
+    // items.map((rem)=>console.log(rem))
+    // console.log("items are")
+     //console.log(state.items)
+
+    },
+    clearCart: (state) => {
+      state.items.length = 0;
+    },
+  },
 });
 
 export default cartSlice.reducer;
-export const{addItems, removeItems,clearcart}= cartSlice.actions;
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
