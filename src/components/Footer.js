cb259376093705672745f897fc15f09f8f2aea4d
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-30 23:53:18 +0530

    Created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer=()=>{
+return(
+    <div className="border border-t-2 border-black bg-[#2A2A2A] h-28 flex justify-center items-center gap-1 bottom-0 w-full">
+    <span className="text-lg text-white tracking-wider"> Copyright @2024; Designed By</span>
+    <span className="text-lg  font-bold text-slate-100 tracking-wider">Harshita Paliwal (^_^)</span>
+    </div>
+)
+}
+export default Footer;
