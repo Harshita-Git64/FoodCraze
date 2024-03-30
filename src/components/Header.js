a09b0572627d02bc5b78da48417a5a0c2723c535
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-03-30 23:56:45 +0530

    Update UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,46 @@
 import { useContext, useEffect, useState } from "react";
-import { LOGO_URL } from "../utils/constants";// way to import for named exports
+import { LOGO_URL } from "../utils/constants"; // way to import for named exports
 import useOffline from "../utils/useOffline";
 import { Link } from "react-router-dom";
 import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-    const [loginbtn,SetLoginBtn]=useState("Login");
-    const [onbtn,SetONBtn]=useState("ON");
-    useEffect(()=>{
-        console.log("useeffect is called1")
-    },[])
-    const status =useOffline();
-    const{LoggedInUser}=useContext(UserContext)
-    //subscribing to store using selector
-    const cartItem=useSelector((stores)=>stores.cart.items)
-    console.log(cartItem)
-    return (
-        <div className="flex justify-between shadow bg-orange-300">
-            <div className="w-[100px]">
-                <img className="" src={LOGO_URL}></img>
-            </div>
-            <div className="flex items-center">
-                <ul className="flex p-5 ">
-                    <li className="mx-3 ">Status :{status?"🟢":"🔴"}</li>
-                    <li className="mx-3 "><Link to="/">Home</Link></li>
-                    <li className="mx-3 "><Link to="/about">AboutUs</Link></li>
-                    <li className="mx-3 "><Link to="/contact">Contact</Link></li>
-                    <li className="mx-3 "><Link to="/cart">Cart({cartItem.length})</Link></li>
-                    <li className="mx-3 "><Link to="/grocery">Grocery</Link></li>
-                    <button onClick={()=>{
-                    loginbtn=="Login"?SetLoginBtn("Logout"):SetLoginBtn("Login");
-                    }}>{loginbtn}</button> 
+  
+  useEffect(() => {
+    console.log("useeffect is called");
+  }, []);
+  const status = useOffline();
+  const { LoggedInUser } = useContext(UserContext);
+  //subscribing to store using selector
+  const cartItem = useSelector((stores) => stores.cart.items);
+  console.log(cartItem);
+  return (
+    <div className="flex justify-between shadow-lg bg-white fixed w-full z-30 top-0">
+      <div className="w-[100px]">
+        <img className="" src={LOGO_URL}></img>
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-5 ">
+          <li className="mx-3 font-medium hover:text-orange-500 transition-all">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="mx-3 font-medium hover:text-orange-500 transition-all">
+            <Link to="/about">AboutUs</Link>
+          </li> 
+          <li className="mx-3 font-medium hover:text-orange-500 transition-all">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="mx-3 font-medium hover:text-orange-500 transition-all">
+            <Link to="/cart">Cart[{cartItem.length}]</Link>
+          </li>
+          <li className="mx-3 ">Status :{status ? "🟢" : "🔴"}</li>
+          {/* <li className="mx-3 font-medium hover:text-orange-500"><Link to="/grocery">Grocery</Link></li> */}
+          <li className="mx-3">{LoggedInUser}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-                    <button onClick={()=>{
-                    onbtn=="ON"?SetONBtn("OFF"):SetONBtn("ON");
-                    }}>{onbtn}</button> 
-                    <li className="mx-3">{LoggedInUser}</li>
-                </ul>
-               
-            </div>
-           
-        </div>
-    )
-    } 
-
-export default Header;    
+export default Header;
