2e30c9c43e385434c07926a861ce87b6b665f106
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-04-01 23:56:30 +0530

    Add remove cart item feature

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -9,18 +9,9 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
       state.items.push(action.payload);
     },
-    removeItem: (state,action) => {
-      console.log(action.payload)
-      const index=state.items.findIndex((item)=>item.id===action.payload.id)
-      if(index>=0){
-      state.items.splice(index,1)
-      }
-      console.log(index)
-      //state.items.filter((rem)=>rem.card.info.id!=action.payload)
-    // items.map((rem)=>console.log(rem))
-    // console.log("items are")
-     //console.log(state.items)
-
+    removeItem: (state, action) => {
+      console.log(action.payload);
+      state.items.splice(action.payload, 1);
     },
     clearCart: (state) => {
       state.items.length = 0;
