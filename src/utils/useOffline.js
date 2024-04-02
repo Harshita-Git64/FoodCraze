1d050e9bace573ebd9b2f04092017120015d72ad
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-04-02 20:16:14 +0530

    fix formating

--- src/utils/useOffline.js
+++ src/utils/useOffline.js
@@ -1,20 +1,15 @@
 import { useEffect, useState } from "react";
 //customhook
-const useOffline=()=>{
-    const[status,Setstatus] = useState(true);
-
-    useEffect(()=>{
-
-
-        window.addEventListener("online",()=>{
-            Setstatus(true)
-        })
-        window.addEventListener("offline",()=>{
-            Setstatus(false)
-        })
-      
-      
-    },[])
-    return status;
-}
+const useOffline = () => {
+  const [status, Setstatus] = useState(true);
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      Setstatus(true);
+    });
+    window.addEventListener("offline", () => {
+      Setstatus(false);
+    });
+  }, []);
+  return status;
+};
 export default useOffline;
