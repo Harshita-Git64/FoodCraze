4e64fa46ca7528b3c839e96ce3f2aae968b8ec93
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-04-02 20:04:02 +0530

    Created Error Page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,16 +1,23 @@
-import { useRouteError } from "react-router-dom"
-const Error=()=>{
+import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Error = () => {
   const error = useRouteError();
-  console.log(error)
+  console.log(error);
   return (
-    
-    <div>
-        <h1>Opps!!</h1>
-        <h2>Something Went Wrong</h2>
-        <h3>error  {error.status} Page {error.statusText}!!</h3>
+    <div className="flex flex-col justify-center items-center content-center gap-y-2 mt-32">
+      <img src="https://seosherpa.com/wp-content/uploads/2020/12/404-error-page-header-transparent.png"></img>
+      {/* <h3>error  {error.status} Page {error.statusText}!!</h3> */}
+      <div className="text-gray-600">
+        This page isn't available!! Try searching for something else.
+      </div>
+      <Link to="/">
+        <button className="px-5 py-2 rounded-full bg-red-600 font-semibold text-white my-3">
+          GO TO HOME
+        </button>
+      </Link>
     </div>
+  );
+};
 
-  )
-}
-
-export default Error
+export default Error;
