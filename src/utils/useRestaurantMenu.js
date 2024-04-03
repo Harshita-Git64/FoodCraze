b5a81ab3f55c3ae5bafbf9a85b2173e2d4bb5501
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-04-03 23:56:09 +0530

    Format the file

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react"
-import {REST_MENU} from "./constants"
+import { useEffect, useState } from "react";
+import { REST_MENU } from "./constants";
 
 //customhook
-const useRestaurantMenu=(resId)=>{
+const useRestaurantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(REST_MENU + resId);
+    const jsondata = await data.json();
+    console.log(jsondata.data);
+    setResInfo(jsondata.data);
+  };
 
-    const[resInfo,setResInfo]=useState(null)
-
-    useEffect(()=>{
-        fetchData()
-    },[])
-    const fetchData= async ()=>{
-        const data= await fetch(REST_MENU+resId)
-        const jsondata=await data.json();
-        console.log(jsondata.data)
-        setResInfo(jsondata.data)
-    }
-    
-   return resInfo;
-}
+  return resInfo;
+};
 export default useRestaurantMenu;
